74bf6c171a3105a59845026537c1e2f1a31c2da5
Your Name <abedoulaye@DESKTOP-MB4JS6O.localdomain>
2026-06-13 05:38:42 -0400

    designed the ui

---
+++ script.js
@@ -0,0 +1,9 @@
+const storyContainer = document.querySelector(".story-container");
+
+const scaryStoryBtn = document.querySelector(".scary-btn");
+const funnyStoryBtn = document.querySelector(".funny-btn");
+const adventureStoryBtn = document.querySelector(".adv-btn");
+const mysteriousStoryBtn = document.querySelector(".myst-btn")
+const sciFiBtn = document.querySelector(".sci-fi-btn")
+
+const resultParagraph = document.getElementById("result");
